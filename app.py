5ed58d6f33465e8ef79df4b46cf8687db8517b41
sanchay21 <sanchaysingh21022007@gmail.com>
2024-08-24 17:25:21 +0530

    creating db

--- app.py
+++ app.py
@@ -2,8 +2,23 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__ , template_folder='templates', static_folder='static', static_url_path='/')
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///moneytrack.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class user(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    data_type = db.Column(db.String(15), nullable=False)
+    amount = db.Column(db.Integer)
+    desc = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"{self.sno}. {self.data_type} - {self.amount}"
 
 @app.route('/')
 def index():
