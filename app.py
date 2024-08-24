d1c2f680f491780bf5afdc8c2b686db119369da1
sanchay21 <sanchaysingh21022007@gmail.com>
2024-08-24 23:26:21 +0530

    signup, home backend

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask import session
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///moneytrack.db"
@@ -10,7 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 app.app_context().push()
-
+app.secret_key = 'the random string'
 class moneydata(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     data_type = db.Column(db.String(15), nullable=False)
@@ -32,18 +33,47 @@ class user(db.Model):
 def index():
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    if request.method == 'POST':
+        username=request.form['username']
+        password=request.form['password']
+
+        register=user(uname=username, upass=password)
+        db.session.add(register)
+        db.session.commit()
+
+        session['username']=username
+        session['password']=password
+        return redirect('home')
+    else:
+        return render_template('signup.html')
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
+    username=session['username']
+    password=session['password']
+    print(username)
+
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        return f"{username}, {password}"
-    else:
-        return "Go To Signup Page"
+        data_type=request.form['data_type']
+        amount=request.form['amount']
+        desc=request.form['desc']
+        NewMoneyData=moneydata(data_type=data_type, amount=amount, desc=desc)
+        db.session.add(NewMoneyData)
+        db.session.commit()
+    moneyData = moneydata.query.all()
+    
+    return render_template('home.html', moneyData=moneyData)
+    
+@app.route('/showdata')
+def showdata(): 
+    userData = user.query.all()
+    return render_template('home.html', userData=userData)
+
+@app.route('/clear_session')
+def clear_sess():
+    session.clear()
 
 if __name__ == "__main__":
     app.run(debug=True, port=1000) 
